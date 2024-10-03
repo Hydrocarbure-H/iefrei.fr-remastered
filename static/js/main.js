c6be373e39e6914bf644e4d4c66939b0a9dd2d17
Thomas Peugnet <97756028+Hydrocarbure-H@users.noreply.github.com>
2024-10-03 13:39:54 +0200

    feat(ui): update dropdown menu for semester selection in main.js and fix trailing newline in components.css

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,35 @@
-/*
-    Copy the link of the course to the clipboard
+/**
+ * Manage the dropdown menu choices for the semesters
+ */
+document.addEventListener("DOMContentLoaded", function ()
+{
+        const currentUrl = window.location.href;
+
+        if (currentUrl.includes('/semester/5'))
+        {
+            setDropbtnText('Semestre 5');
+        }
+        else if (currentUrl.includes('/semester/6'))
+        {
+            setDropbtnText('Semestre 6');
+        }
+        else if (currentUrl.includes('/semester/7'))
+        {
+            setDropbtnText('Semestre 7');
+        }
+        else if (currentUrl.includes('/semester/8'))
+        {
+            setDropbtnText('Semestre 8');
+        }
+        else
+        {
+            setDropbtnText('Tous les cours');
+        }
+});
+
+
+/**
+ * Copy the link of the course to the clipboard
  */
 function copy_link(id, title)
 {
@@ -10,18 +40,36 @@ function copy_link(id, title)
     });
 }
 
-function load_semester(semester)
+/**
+ * Set the text of the dropdown button
+ * @param text
+ */
+function setDropbtnText(text)
 {
     const dropbtn = document.querySelector('.dropbtn');
-
-    if (semester === 'all')
-    {
-        dropbtn.innerHTML = 'Tous les cours <ion-icon name="chevron-down-outline"></ion-icon>';
-        window.location.href = "{{ url_for('index') }}";
-    }
-    else
-    {
-        dropbtn.innerHTML = 'Semestre ' + semester + ' <ion-icon name="chevron-down-outline"></ion-icon>';
-        window.location.href = "{{ url_for('courses_by_semester', semester=semester) }}";
-    }
+    dropbtn.innerHTML = text + ' <ion-icon name="chevron-down-outline"></ion-icon>';
 }
+
+/**
+ * Load the course in the iframe
+ * @param courseUrl
+ */
+function loadCourse(courseUrl)
+{
+
+    // If the screen is small (800px), redirect to the course page
+    if (window.innerWidth < 800) {
+        window.location.href = courseUrl;
+    }
+
+    // Update classes for the main divs
+    document.getElementById('main_left').classList.remove('full-width');
+    document.getElementById('main_left').classList.add('half-width');
+
+    document.getElementById('main_right').classList.remove('hidden');
+    document.getElementById('main_right').classList.add('visible');
+
+    // Update the iframe src
+    document.getElementById('viewer').src = courseUrl;
+}
+
